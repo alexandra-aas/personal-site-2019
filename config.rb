74a89d4d1cd97a7b89da5dbac7ebea08735a5abd
Ali Aas <alexandra.aas@upstatement.com>
2019-02-11 21:12:03 -0500

    Setup

---
+++ config.rb
@@ -0,0 +1,21 @@
+activate :autoprefixer
+
+set :relative_links, true
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+set :layout, "layouts/application"
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+page "/feed.xml", layout: false
+
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  activate :relative_assets
+end
