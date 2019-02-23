63c890970c6d49c5a0dbb98e64f3f030d2ac8628
Ali Aas <alexandra.aas@upstatement.com>
2019-02-23 09:35:51 -0500

    WIP

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,7 @@
-// This is where it all goes :)
+$('[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var target = this.hash;
+    var navHeight = $('nav').height();
+    var offset = $(target).offset().top - navHeight;
+    $('html, body').stop().animate({ 'scrollTop':  offset }, 900 );
+});
